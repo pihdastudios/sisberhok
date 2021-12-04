a7d416688fca9d6dfc5d297f87bf58b25b462102
abc <abc@example.com>
2021-12-04 19:05:57 +0700

    init

---
+++ include/main.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include "esp_system.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "esp_vfs_dev.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+
+#include "mqtt_client.h"
+
+#define MQTT_PUB_TEMP "esp32/dht/temperature"
+#define MQTT_PUB_HUM "esp32/dht/humidity"
+
+// #include "cJSON.h"
+
+// #include "DHT.hpp"
+
+#define LED_PIN GPIO_NUM_4
+#define DHT_PIN GPIO_NUM_23
+#define BTN_PIN GPIO_NUM_2
+
+#define EXAMPLE_ESP_WIFI_SSID "Redmi"
+#define EXAMPLE_ESP_WIFI_PASS "syafiq07"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 5
+
+/* FreeRTOS event group to signal when we are connected*/
+static EventGroupHandle_t s_wifi_event_group;
+
+/* The event group allows multiple bits for each event, but we only care about two events:
+ * - we are connected to the AP with an IP
+ * - we failed to connect after the maximum amount of retries */
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+TaskHandle_t ISR = nullptr;
+static bool led_status = false;
+
+esp_mqtt_client_handle_t global_client = nullptr;
+
+static SemaphoreHandle_t mutex;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data);
+static void log_error_if_nonzero(const char *message, int error_code);
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data);
+static void mqtt_app_start();
+static void mqtt_set_default();
+
+static int s_retry_num = 0;
+
+const char TAG[] = "MQTT_EXAMPLE";
+
+std::string ip_address;
+
+void DHT_task(void *pvParameter);
+void IRAM_ATTR button_isr_handler(void *pvParameter);
+void button_task(void *pvParameter);
+void serial_task(void *pvParameter);
+void init_gpio();
+void init_wifi();
+
+#define MQTT_ADDRESS "mqtt://broker.emqx.io:1883"
+
+extern "C"
+{
+    void app_main();
+}
---
+++ src/main.cpp
@@ -0,0 +1,294 @@
+#include "main.hpp"
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+    if (error_code != 0)
+    {
+        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+    }
+}
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG, "connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+
+        char buff[16];
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        sprintf(buff, IPSTR, IP2STR(&event->ip_info.ip));
+        ip_address = std::string(buff);
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void DHT_task(void *pvParameter)
+{
+    // DHT dht;
+    // dht.setDHTgpio(DHT_PIN);
+    // while (1)
+    // {
+    //     int ret = dht.readDHT();
+    //     if (ret == DHT_OK)
+    //     {
+    //         esp_mqtt_client_publish(global_client, "/esp32/dht/temperature", std::to_string(dht.getTemperature()).c_str(), 0, 1, 0);
+    //         esp_mqtt_client_publish(global_client, "/esp32/dht/humidity", std::to_string(dht.getHumidity()).c_str(), 0, 1, 0);
+
+    //         vTaskDelay(2000 / portTICK_RATE_MS);
+    //     }
+    // }
+}
+
+// interrupt service routine, called when the button is pressed
+void IRAM_ATTR button_isr_handler(void *pvParameter)
+{
+    xTaskResumeFromISR(ISR);
+}
+
+// task that will react to button clicks
+void button_task(void *pvParameter)
+{
+    while (1)
+    {
+        vTaskSuspend(nullptr);
+        static int64_t last_interrupt_time = 0;
+        int64_t interrupt_time = esp_timer_get_time();
+        if (interrupt_time - last_interrupt_time > 200000)
+        {
+            ESP_LOGI(TAG, "interrupt");
+            last_interrupt_time = interrupt_time;
+            led_status = !led_status;
+            esp_mqtt_client_publish(global_client, "sisberhok/esp/lamp", led_status ? "true" : "false", 0, 1, 0);
+            gpio_set_level(LED_PIN, led_status);
+        }
+    }
+}
+
+void init_gpio()
+{
+    gpio_pad_select_gpio(BTN_PIN);
+    gpio_pad_select_gpio(LED_PIN);
+
+    // set the correct direction
+    gpio_set_direction(BTN_PIN, GPIO_MODE_INPUT);
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+
+    //set pullup mode
+    gpio_set_pull_mode(BTN_PIN, GPIO_PULLUP_ONLY);
+
+    // enable interrupt on falling (1->0) edge for button pin
+    gpio_set_intr_type(BTN_PIN, GPIO_INTR_NEGEDGE);
+
+    // Install the driver’s GPIO ISR handler service, which allows per-pin GPIO interrupt handlers.
+    // install ISR service with default configuration
+    gpio_install_isr_service(0);
+
+    // attach the interrupt service routine
+    gpio_isr_handler_add(BTN_PIN, button_isr_handler, nullptr);
+}
+
+void init_wifi()
+{
+    s_wifi_event_group = xEventGroupCreate();
+    esp_netif_init();
+
+    esp_event_loop_create_default();
+    esp_netif_create_default_wifi_sta();
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    esp_event_handler_instance_register(WIFI_EVENT,
+                                        ESP_EVENT_ANY_ID,
+                                        &event_handler,
+                                        NULL,
+                                        &instance_any_id);
+    esp_event_handler_instance_register(IP_EVENT,
+                                        IP_EVENT_STA_GOT_IP,
+                                        &event_handler,
+                                        NULL,
+                                        &instance_got_ip);
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {};
+    strcpy((char *)wifi_config.sta.ssid, EXAMPLE_ESP_WIFI_SSID);
+    strcpy((char *)wifi_config.sta.password, EXAMPLE_ESP_WIFI_PASS);
+    wifi_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    wifi_config.sta.pmf_cfg.capable = true;
+    wifi_config.sta.pmf_cfg.required = false;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                           WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                           pdFALSE,
+                                           pdFALSE,
+                                           portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else
+    {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+
+    esp_event_handler_instance_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, instance_got_ip);
+    esp_event_handler_instance_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, instance_any_id);
+    vEventGroupDelete(s_wifi_event_group);
+}
+
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+    ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+    esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
+    esp_mqtt_client_handle_t client = event->client;
+    int msg_id;
+    switch ((esp_mqtt_event_id_t)event_id)
+    {
+    case MQTT_EVENT_CONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+        msg_id = esp_mqtt_client_subscribe(client, "sisberhok/esp/toggle", 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+
+        break;
+    case MQTT_EVENT_DISCONNECTED:
+        ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+        break;
+
+    case MQTT_EVENT_SUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+        msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+        ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+        break;
+    case MQTT_EVENT_UNSUBSCRIBED:
+        ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_PUBLISHED:
+        ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+        break;
+    case MQTT_EVENT_DATA:
+        ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+        printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+        printf("DATA=%.*s\r\n", event->data_len, event->data);
+        led_status = !led_status;
+        gpio_set_level(LED_PIN, led_status);
+        break;
+    case MQTT_EVENT_ERROR:
+        ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+        if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+        {
+            log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+            log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+            log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+            ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+        }
+        break;
+    default:
+        ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+        break;
+    }
+}
+
+static void mqtt_app_start()
+{
+    esp_mqtt_client_config_t mqtt_cfg = {};
+    mqtt_cfg.uri = MQTT_ADDRESS;
+    global_client = esp_mqtt_client_init(&mqtt_cfg);
+
+    /* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+    esp_mqtt_client_register_event(global_client, (esp_mqtt_event_id_t)ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+    esp_mqtt_client_start(global_client);
+}
+
+static void mqtt_set_default()
+{
+    esp_mqtt_client_publish(global_client, "sisberhok/esp/lamp", led_status ? "true" : "false", 0, 1, 0);
+    esp_mqtt_client_publish(global_client, "sisberhok/esp/ipaddr", ip_address.c_str(), 0, 1, 0);
+}
+
+void app_main()
+{
+
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
+
+    esp_log_level_set("*", ESP_LOG_INFO);
+    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
+    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
+    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
+    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
+    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    mutex = xSemaphoreCreateMutex();
+
+    init_gpio();
+    init_wifi();
+    mqtt_app_start();
+    mqtt_set_default();
+
+    // Create and start stats task
+    xTaskCreate(&button_task, "button_task", 4096, nullptr, 20, &ISR);
+    // xTaskCreate(&DHT_task, "DHT_task", 2048, nullptr, 5, nullptr);
+}
